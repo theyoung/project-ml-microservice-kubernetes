5db34c9b3926fc692bfd6b6c6df3e82645e9ddbf
steven <theyoung2002@naver.com>
2021-03-08 14:52:42 +0000

    commit project

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="stevenna/mlmicro"
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag stevenna/mlproject:latest $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
